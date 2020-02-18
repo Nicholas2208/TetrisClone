ffaf87525be472befd5ada56c16199bd3c319c83
Nicholas2208 <nikolaj.belyakov.84@mail.ru>
2020-02-18 21:53:18 +0300

    test

---
+++ src/gui/Main.java
@@ -0,0 +1,30 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
+		ResourceBundle resource = null;
+		FXMLLoader fxmlLoader = new FXMLLoader(location, resource);
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root, 400, 510);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
